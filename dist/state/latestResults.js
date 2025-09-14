17e0956dbeb5c4d9027167cb1cd27c04f8497fc5
tcoders16 <tcoders08164002@gmail.com>
2025-09-14 16:47:01 -0400

    fixed

--- dist/state/latestResults.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.latestByIp = void 0;
-exports.latestByIp = new Map();
-//# sourceMappingURL=latestResults.js.map
--- dist/types/events.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// types/events.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asISO = void 0;
-/** Helper to brand a string after validation (runtime guard lives in zod schema) */
-const asISO = (s) => s;
-exports.asISO = asISO;
-//# sourceMappingURL=events.js.map
